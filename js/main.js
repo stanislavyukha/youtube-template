3345ca65bc3db25bd40f960b25cf54e6df34ddd2
Stas Yukha <valhalla245@gmail.com>
2021-04-08 16:57:00 +0300

    Aded main content section

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const swiper1 = new Swiper('.channel-slider-1', {
+  
+   loop: true,
+   slidesPerView: 6,
+   navigation: {
+     nextEl: '.channel-button-next-1',
+     prevEl: '.channel-button-prev-1',
+   },
+ 
+ });
+
+ const swiper2 = new Swiper('.channel-slider-2', {
+  
+  loop: true,
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.channel-button-next-2',
+    prevEl: '.channel-button-prev-2',
+  },
+
+});
+
+const swiper3 = new Swiper('.channel-slider-3', {
+  
+  loop: true,
+  slidesPerView: 6,
+  navigation: {
+    nextEl: '.channel-button-next-3',
+    prevEl: '.channel-button-prev-3',
+  },
+
+});
+ 
